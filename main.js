32d30db180108d3fb3078614799bfc48f9148d1e
akshaj-legorobot <84026684+akshaj-legorobot@users.noreply.github.com>
2022-07-22 16:50:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialized');
 }
 
 function draw() {
@@ -20,4 +26,14 @@ function draw() {
 
 function take_snapshot() {
     save('myfilterimage.png');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log('nose x = ' + noseX);
+        console.log('nose y = ' + noseY);
+    }
 }
